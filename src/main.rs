5cd13483e346c397efa78f0e811e5e4e0042ae06
Alex Alikiotis <alexalikiotis5@gmail.com>
2021-01-11 17:57:25 +0200

    making snake move to direction

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,84 @@
 use ggez::conf;
 use ggez::event;
 use ggez::graphics;
+use ggez::timer;
 use ggez::{Context, ContextBuilder, GameResult};
 
-struct MainState {}
+struct GameState {
+    snake: Snake,
+}
 
-impl MainState {
+impl GameState {
     fn new() -> Self {
-        MainState {}
+        GameState {
+            snake: Snake::new(),
+        }
     }
 }
 
-impl event::EventHandler for MainState {
+impl event::EventHandler for GameState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        // clear background
+        // clear window buffer
         graphics::clear(ctx, graphics::WHITE);
-
-        // draw square
-        let square = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            graphics::Rect::new(0.0, 0.0, 25.0, 25.0),
-            graphics::Color::new(1.0, 0.0, 0.0, 1.0),
-        )?;
-        graphics::draw(ctx, &square, graphics::DrawParam::default())?;
-
+        // render snake
+        self.snake.draw(ctx)?;
         // commit changes to window
         graphics::present(ctx)
     }
 
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        const TARGET_FPS: u32 = 8;
+        // update game 8 times per second
+        while timer::check_update_time(ctx, TARGET_FPS) {
+            self.snake.update(ctx)?;
+        }
         Ok(())
     }
+}
+
+struct Snake {
+    pos_x: i32,
+    pos_y: i32,
+    dir: Direction,
+}
+
+impl Snake {
+    fn new() -> Snake {
+        Snake {
+            pos_x: 0,
+            pos_y: 0,
+            dir: Direction::Right,
+        }
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        // draw the head os snake
+        let square = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            graphics::Rect::new_i32(self.pos_x * 25, self.pos_y * 25, 25, 25),
+            graphics::Color::from_rgb(255, 0, 0),
+        )?;
+
+        graphics::draw(ctx, &square, graphics::DrawParam::default())
+    }
+
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        match self.dir {
+            Direction::Left => self.pos_x -= 1,
+            Direction::Right => self.pos_x += 1,
+            Direction::Up => self.pos_y -= 1,
+            Direction::Down => self.pos_y += 1,
+        };
+        Ok(())
+    }
+}
+
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
 }
 
 fn main() -> GameResult {
@@ -46,6 +93,6 @@ fn main() -> GameResult {
         .build()?;
 
     // initialize game state and run game-loop
-    let state = &mut MainState::new();
+    let state = &mut GameState::new();
     event::run(ctx, event_loop, state)
 }
