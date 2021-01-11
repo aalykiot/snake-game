82843481c42cd665c603178cc98784153c511f52
Alex Alikiotis <alexalikiotis5@gmail.com>
2021-01-11 11:45:56 +0200

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,17 @@ impl event::EventHandler for MainState {
 }
 
 fn main() -> GameResult {
-    let setup = conf::WindowSetup::default().title("Snake Game");
-    let mode = conf::WindowMode::default().dimensions(500_f32, 500_f32);
-
-    let (ctx, event_loop) = &mut ContextBuilder::new("snake_game", "ggez")
-        .window_setup(setup)
-        .window_mode(mode)
+    // create game context
+    let (ctx, event_loop) = &mut ContextBuilder::new("snake_game", "aa-studios")
+        .window_setup(conf::WindowSetup::default().title("Snake Game"))
+        .window_mode(
+            conf::WindowMode::default()
+                .dimensions(500_f32, 500_f32)
+                .resizable(false),
+        )
         .build()?;
 
+    // initialize game state and run game-loop
     let state = &mut MainState::new();
-
     event::run(ctx, event_loop, state)
 }
